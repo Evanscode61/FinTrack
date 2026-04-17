fb81e0fda6a6e8ee65a5aecd91e0adc38730bf7c
Evanscode61 <evancekibe53@gmail.com>
2026-04-17 12:53:38 +0300

    "feat: add custom User model with email authentication"

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from Base.models import BaseModel
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def get_by_natural_key(self, email):
+        return self.get(email=email)
+
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    email        = models.EmailField(max_length=100, unique=True)
+    first_name   = models.CharField(max_length=100)
+    last_name    = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11, unique=True, blank=True, null=True)
+    profile_pic  = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    is_active    = models.BooleanField(default=True)
+    is_staff     = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD  = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.email
